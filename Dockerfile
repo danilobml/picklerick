bab27bc3fb195632da8ae19ef929f672528a25ef
danilobml <danbarml@gmail.com>
2024-04-25 20:56:25 +0200

    add: connection to postgresql db; db created in docker-compose

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,8 @@ FROM python:3.12-slim
 WORKDIR /picklerick
 COPY . .
 
+RUN apt-get update \
+    && apt-get -y install libpq-dev gcc
+
 RUN pip3 install -U pip
 RUN pip3 install -r requirements.txt
--- picklerick/settings.py
+++ picklerick/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'picklerick.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "picklerick",
+        "USER": "picklerick",
+        "PASSWORD": "picklerick",
+        "HOST": "postgres",
+        "PORT": "5432",
     }
 }
 
