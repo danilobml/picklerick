1323a203d532f5d01819a4ba78da5ca214a96797
danilobml <danbarml@gmail.com>
2024-04-26 22:52:07 +0200

    modify: tests; add: tests error cases

--- ricks/models.py
+++ ricks/models.py
@@ -5,5 +5,5 @@ class Rick(models.Model):
     """
         A model for our infinite Ricks
     """
-    universe = models.CharField(max_length=255)
+    universe = models.CharField(max_length=255, unique=True)
     is_morty_alive = models.BooleanField()
--- ricks/tests.py
+++ ricks/tests.py
@@ -3,55 +3,111 @@ from rest_framework.test import APITestCase
 from .models import Rick
 from django.urls import reverse
 from rest_framework import status
-from .serializers import RickSerializer
+# from .serializers import RickSerializer
 
 
 class RickTestCase(APITestCase):
     """
         Test for CRUD functionalities for the 'ricks' endpoint
     """
-    def setUp(self):
-        self.new_rick1 = Rick.objects.create(universe="t380", is_morty_alive=True)
-        self.new_rick2 = Rick.objects.create(universe="t490", is_morty_alive=False)
 
     def test_get_all_ricks(self):
+        Rick.objects.create(universe="t380", is_morty_alive=True)
+        Rick.objects.create(universe="t490", is_morty_alive=False)
         response = self.client.get(reverse('ricks-list'))
-        ricks = Rick.objects.all()
-        serializer = RickSerializer(ricks, many=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(len(response.data), 2)
 
     def test_get_one_rick(self):
-        response = self.client.get(reverse('ricks-detail', args=(self.new_rick1.id,)))
-        rick = Rick.objects.get(pk=self.new_rick1.id)
-        serializer = RickSerializer(rick)
+        new_rick = Rick.objects.create(universe="t380", is_morty_alive=True)
+        response = self.client.get(reverse('ricks-detail', args=(new_rick.id,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.data, {
+            "id": new_rick.id,
+            "universe": new_rick.universe,
+            "is_morty_alive": new_rick.is_morty_alive
+            })
+
+    def test_get_one_rick_non_existent_id(self):
+        new_rick = Rick.objects.create(universe="t380", is_morty_alive=True)
+        response = self.client.get(reverse('ricks-detail', args=(new_rick.id+1,)))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_create_one_rick(self):
+        self.assertFalse(Rick.objects.filter(universe="t590").exists())
         data = {
             "universe": "t590",
             "is_morty_alive": False
         }
         response = self.client.post(reverse('ricks-list'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        rick = Rick.objects.filter(universe="t590").first()
-        all_ricks = Rick.objects.all()
-        self.assertIn(rick, all_ricks)
+        self.assertTrue(Rick.objects.filter(universe="t590").exists())
+
+    def test_create_one_rick_no_data(self):
+        data = None
+        response = self.client.post(reverse('ricks-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_one_rick_missing_field_data(self):
+        data = {
+            "universe": "",
+            "is_morty_alive": False
+        }
+        response = self.client.post(reverse('ricks-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_update_one_rick(self):
+        new_rick = Rick.objects.create(universe="t380", is_morty_alive=True)
+        self.assertFalse(Rick.objects.filter(universe="t390").exists())
         data = {
             "universe": "t390",
             "is_morty_alive": False
         }
-        response = self.client.put(reverse('ricks-detail', args=(self.new_rick1.id,)), data)
+        response = self.client.put(reverse('ricks-detail', args=(new_rick.id,)), data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        rick = Rick.objects.get(pk=self.new_rick1.id)
-        serializer = RickSerializer(rick)
-        self.assertEqual(serializer.data.get('universe'), "t390")
+        self.assertTrue(Rick.objects.filter(universe="t390").exists())
+
+    def test_update_one_rick_no_data(self):
+        new_rick = Rick.objects.create(universe="t380", is_morty_alive=True)
+        data = {
+            "universe": "",
+            "is_morty_alive": False
+        }
+        response = self.client.put(reverse('ricks-detail', args=(new_rick.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_one_rick_missing_field_data(self):
+        new_rick = Rick.objects.create(universe="t380", is_morty_alive=True)
+        data = {
+            "universe": "",
+            "is_morty_alive": False
+        }
+        response = self.client.put(reverse('ricks-detail', args=(new_rick.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_one_rick_non_existent_id(self):
+        new_rick = Rick.objects.create(universe="t380", is_morty_alive=True)
+        self.assertFalse(Rick.objects.filter(universe="t390").exists())
+        data = {
+            "universe": "t390",
+            "is_morty_alive": False
+        }
+        response = self.client.put(reverse('ricks-detail', args=(new_rick.id+1,)), data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_delete_one_rick(self):
-        response = self.client.delete(reverse('ricks-detail', args=(self.new_rick1.id,)))
+        new_rick = Rick.objects.create(universe="t590", is_morty_alive=True)
+        self.assertTrue(Rick.objects.filter(universe="t590").exists())
+
+        response = self.client.delete(reverse('ricks-detail', args=(new_rick.id,)))
+
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        with self.assertRaises(Rick.DoesNotExist):
-            Rick.objects.get(pk=self.new_rick1.id)
+        self.assertFalse(Rick.objects.filter(universe="t590").exists())
+
+    def test_delete_one_rick_non_existent_id(self):
+        new_rick = Rick.objects.create(universe="t590", is_morty_alive=True)
+        self.assertTrue(Rick.objects.filter(universe="t590").exists())
+
+        response = self.client.delete(reverse('ricks-detail', args=(new_rick.id+1,)))
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
