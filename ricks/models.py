96fc787a5426f89c01392ed9528a32dbeb7f039d
danilobml <danbarml@gmail.com>
2024-04-26 23:18:49 +0200

    modify: remove unique condition

--- ricks/models.py
+++ ricks/models.py
@@ -5,5 +5,5 @@ class Rick(models.Model):
     """
         A model for our infinite Ricks
     """
-    universe = models.CharField(max_length=255, unique=True)
+    universe = models.CharField(max_length=255)
     is_morty_alive = models.BooleanField()
