369b2dad59ad982358aaaf26d2f06b65fac5639d
danilobml <danbarml@gmail.com>
2024-04-29 18:21:09 +0200

    add: Morty model and migrations; modify: Rick model

--- ricks/models.py
+++ ricks/models.py
@@ -6,4 +6,3 @@ class Rick(models.Model):
         A model for our infinite Ricks
     """
     universe = models.CharField(max_length=255, unique=True)
-    is_morty_alive = models.BooleanField(default=True)
