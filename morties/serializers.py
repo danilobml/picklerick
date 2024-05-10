5089e7c26628e5f04920f909c0d90924891316d2
danilobml <danbarml@gmail.com>
2024-05-10 16:24:54 +0200

    modify: remove redundant line

--- morties/serializers.py
+++ morties/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from .models import Morty
-from ricks.models import Rick
-from django.shortcuts import get_object_or_404
 
 
 class MortySerializer(serializers.ModelSerializer):
@@ -17,7 +15,6 @@ class MortySerializer(serializers.ModelSerializer):
     def validate_paired_rick(self, rick):
         if rick is None:
             return None
-        rick = get_object_or_404(Rick, pk=rick.id)
         if rick.paired_morties.filter(is_alive=True).exists() and self.initial_data.get("is_alive"):
             raise ValidationError({"error": "Rick already has a living paired Morty."})
         return rick
